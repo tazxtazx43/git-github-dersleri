ea909de26504e809e9827920de3c5a20fd2217fe
tazxtazx <tazxtazx@tazxcorp.com>
2024-06-01 07:00:43 +0300

    ilk sayının ikinci sayıdan büyük olması gerektiğini bildiren mesaj ve kontrol eklendi

--- cikarma.js
+++ cikarma.js
@@ -1,5 +1,10 @@
 function cikar() {
-	let num1 = parseInt(sayi1.value);
-    let num2 = parseInt(sayi2.value);
-    sonuc.innerHTML = num1- num2;
+    let num1 = parseInt(document.getElementById('sayi1').value);
+    let num2 = parseInt(document.getElementById('sayi2').value);
+    
+    if (num1 < num2) {
+        alert("Çıkarma işlemi için birinci sayının ikinci sayıdan büyük olması gerekiyor!");
+    } else {
+        document.getElementById('sonuc').innerHTML = num1 - num2;
+    }
 }
